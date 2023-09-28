69d519249131b5e5bbca7b4e5d896e77ba66d18d
Vikalp Mishra <mishravikalp896@gmail.com>
2023-09-28 15:18:14 +0530

    some work complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-function App() {
- 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Header from "./Component/Header"
+import CoinDetails from "./Component/CoinDetails"
+import Coins from "./Component/Coins"
+import Exchange from "./Component/Exchange"
+import Home from "./Component/Home"
 
+function App() {
   return (
-    <>
-     <h1>Hello princy janwar hai</h1>
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coins" element={<Coins />} />
+        <Route path="/exchange" element={<Exchange />} />
+        <Route path="/coin/:id" element={<CoinDetails />} />
+      </Routes>
+    </Router>
   )
 }
 
